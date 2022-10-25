7e1c6b7bbaf19644ed4eba19fb8a11f6d5a2c421
cacau <anaclaudia.sza@outlook.com>
2022-10-25 18:53:32 -0300

    feat(task): testa avaliador

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import 'express-async-errors';
+// import swaggerUi from 'swagger-ui-express'
 
 import carRoutes from './routes/CarRoute';
 import motorcycleRoutes from './routes/MotorcycleRoute';
 
 import errorHandler from './middlewares/errorHandler';
 
+// const swaggerFile = require('./swagger.json');
+
 const app = express();
 
 app.use(express.json());
+// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(carRoutes);
 app.use(motorcycleRoutes);
