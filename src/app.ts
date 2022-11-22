25da926b806f75b3e145c387a205bdbeb7e3e797
cacau <anaclaudia.sza@outlook.com>
2022-11-22 13:38:26 -0300

    feat: configuracao swagger-ui-express

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 import 'express-async-errors';
-// import swaggerUi from 'swagger-ui-express'
+import swaggerUi from 'swagger-ui-express';
 
 import carRoutes from './routes/CarRoute';
 import motorcycleRoutes from './routes/MotorcycleRoute';
 
 import errorHandler from './middlewares/errorHandler';
 
-// const swaggerFile = require('./swagger.json');
+import swaggerJson from '../swagger.json';
 
 const app = express();
 
 app.use(express.json());
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.use(carRoutes);
-app.use(motorcycleRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJson));
+app.use('/terms', (_req, res) => { res.status(200).json({ message: 'Termos de Serviço' }); });
+
+app.use('/v1', carRoutes);
+app.use('/v1', motorcycleRoutes);
 
 app.use(errorHandler);
 
